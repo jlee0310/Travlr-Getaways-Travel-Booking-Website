dbe20838e189445e3d9ff958504dec828330da19
Jiwon Lee <sasum84@gmail.com>
2023-01-30 10:14:04 -0500

    Create new application API

--- .seedgooserc.js
+++ .seedgooserc.js
@@ -1,6 +1,6 @@
 
 module.exports = {
-    modelBaseDirectory: 'app_server/database/models', // model directory name
+    modelBaseDirectory: 'app_api/database/models', // model directory name
     models: ['*.js', '!db.js'], // model matcher
     data: 'data', // data directory name
     db: 'mongodb://localhost:27017/travlr' // db connection url
--- app.js
+++ app.js
@@ -1,16 +1,19 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-const hbs = require('hbs'); // I'm not sure
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const hbs = require('hbs');
+require('./app_api/database/db');
 
-var indexRouter = require('./app_server/routes/index');
-var usersRouter = require('./app_server/routes/users');
-var travelRouter = require('./app_server/routes/travel');
+const indexRouter = require('./app_server/routes/index');
+const usersRouter = require('./app_server/routes/users');
+const travelRouter = require('./app_server/routes/travel');
+const apiRouter = require('./app_api/routes/index');
+
 const { hasSubscribers } = require('diagnostics_channel');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'app_server', 'views'));
@@ -30,6 +33,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/travel', travelRouter);
+app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
