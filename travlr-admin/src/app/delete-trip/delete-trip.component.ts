439e1f2413b283cc43f7817741b9cd45050ef027
Jiwon Lee <sasum84@gmail.com>
2023-02-09 11:19:19 -0500

    created delete-trip compos

---
+++ travlr-admin/src/app/delete-trip/delete-trip.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-delete-trip',
+  templateUrl: './delete-trip.component.html',
+  styleUrls: ['./delete-trip.component.css']
+})
+export class DeleteTripComponent {
+
+}
