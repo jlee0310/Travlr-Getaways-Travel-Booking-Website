696160b16b4c318e39875da1eb22719e82417a93
Jiwon Lee <sasum84@gmail.com>
2023-02-14 15:43:07 -0500

    Getting 201 instead of 401

--- app_api/controllers/trips.js
+++ app_api/controllers/trips.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const Trip = mongoose.model("trips");
+const User = mongoose.model("users");
 
 // GET: /trips - lists all the trips
 const tripsList = async (req, res) => {
@@ -45,67 +46,73 @@ const tripsFindCode = async (req, res) => {
 
 // POST: /trips/:tripCode - add a single trip
 const tripsAddTrip = async (req, res) => {
-    Trip
-        .create({
-            code: req.body.code,
-            name: req.body.name,
-            length: req.body.length,
-            start: req.body.start,
-            resort: req.body.resort,
-            perPerson: req.body.perPerson,
-            image: req.body.image,
-            description: req.body.description
-        },
-            (err, trip) => {
-                if (err) {
-                    return res
-                        .status(400)
-                        .json(err);
-                } else {
-                    return res
-                        .status(201)
-                        .json(trip);
-                }
-            });
+    getUser(req, res,
+        (req, res) => {
+            Trip
+                .create({
+                    code: req.body.code,
+                    name: req.body.name,
+                    length: req.body.length,
+                    start: req.body.start,
+                    resort: req.body.resort,
+                    perPerson: req.body.perPerson,
+                    image: req.body.image,
+                    description: req.body.description
+                },
+                    (err, trip) => {
+                        if (err) {
+                            return res
+                                .status(400)
+                                .json(err);
+                        } else {
+                            return res
+                                .status(201)
+                                .json(trip);
+                        }
+                    });
+        });
 }
 
 // PUT: /trips/:tripCode - update a single trip
 const tripsUpdateTrip = async (req, res) => {
-    console.log(req.body);
-    Trip
-        .findOneAndUpdate({ 'code': req.params.tripCode }, {
-            code: req.body.code,
-            name: req.body.name,
-            length: req.body.length,
-            start: req.body.start,
-            resort: req.body.resort,
-            perPerson: req.body.perPerson,
-            image: req.body.image,
-            description: req.body.description
-        }, { new: true })
-        .then(trip => {
-            if (!trip) {
-                return res
-                    .status(404)
-                    .send({
-                        message: "Trip not found with code "
-                            + req.params.tripCode
-                    });
-            }
-            res.send(trip);
-        }).catch(err => {
-            if (err.kind === 'ObjectId') {
-                return res
-                    .status(404)
-                    .send({
-                        message: "Trip not found with code "
-                            + req.params.tripCode
-                    });
-            }
-            return res
-                .status(500) // server error
-                .json(err);
-        });
+    // console.log(req.body);
+    getUser(req, res,
+        (req, res) => {
+            Trip
+                .findOneAndUpdate({ 'code': req.params.tripCode }, {
+                    code: req.body.code,
+                    name: req.body.name,
+                    length: req.body.length,
+                    start: req.body.start,
+                    resort: req.body.resort,
+                    perPerson: req.body.perPerson,
+                    image: req.body.image,
+                    description: req.body.description
+                }, { new: true })
+                .then(trip => {
+                    if (!trip) {
+                        return res
+                            .status(404)
+                            .send({
+                                message: "Trip not found with code "
+                                    + req.params.tripCode
+                            });
+                    }
+                    res.send(trip);
+                }).catch(err => {
+                    if (err.kind === 'ObjectId') {
+                        return res
+                            .status(404)
+                            .send({
+                                message: "Trip not found with code "
+                                    + req.params.tripCode
+                            });
+                    }
+                    return res
+                        .status(500) // server error
+                        .json(err);
+                });
+        })
 }
 
 // DELETE: /trips/- delete a single trip
@@ -119,6 +126,30 @@ const tripsDeleteTrip = async (req, res) => {
                 return res.status(204).json({ message: 'Trip deleted successfully' });
             }
         };
+};
+
+const getUser = (req, res, callback) => {
+    if (req.payload && req.payload.email) {
+        User
+            .findOne({ email: req.payload.email })
+            .exec((err, user) => {
+                if (!user) {
+                    return res
+                        .status(404)
+                        .json({ "message": "User no found" });
+                } else if (err) {
+                    console.log(err);
+                    return res
+                        .status(404)
+                        .json(err);
+                }
+                callback(req, res, user.name);
+            });
+    } else {
+        return res
+            .status(404)
+            .json({ "message": "User not found" });
+    }
 };
 
 module.exports = {
--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -1,18 +1,32 @@
 const express = require("express");
 const router = express.Router();
+const jwt = require('express-jwt');
+const auth = jwt({
+    secret: process.env.JWT_SECRET,
+    userProperty: 'payload'
+});
 
+const authController = require('../controllers/authentication');
 const tripsController = require("../controllers/trips");
+
+router
+    .route('/login')
+    .post(authController.login);
+
+router
+    .route('/register')
+    .post(authController.register);
 
 router
     .route("/trips")
     .get(tripsController.tripsList)
-    .post(tripsController.tripsAddTrip);
-// .delete(tripsController.tripsDeleteTrip);
+    .post(auth, tripsController.tripsAddTrip);
+
 
 router
     .route("/trips/:tripCode")
     .get(tripsController.tripsFindCode)
-    .put(tripsController.tripsUpdateTrip)
+    .put(auth, tripsController.tripsUpdateTrip)
     .delete(tripsController.tripsDeleteTrip);
 
 module.exports = router;
